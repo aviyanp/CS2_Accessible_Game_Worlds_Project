3ac69094fc6dceac0ff3c17adebc46e63a59f70d
Aviyan Panday <aviyanjp@gmail.com>
2025-03-03 10:52:32 -0500

    Co-authored-by: hector-bonnaud <hector-bonnaud@users.noreply.github.com>

--- basicIsland.js
+++ basicIsland.js
@@ -1,19 +1,70 @@
 class basicIsland {
-  constructor(positionx,positiony, above, below, left, right) {
+  constructor(positionx,positiony, above, right, below, left) {
     this.x = positionx;
     this.y = positiony;
     this.acell = above;
-    this.bcell = below;
-    this.lcell = left;
-    this.rcell = right;
+    this.bcell = right;
+    this.ccell = below;
+    this.dcell = left;
+    this.type1 = 1;
+    this.type2 = 1;
   }
   
   show() {
-    fill(100,200,100);
-    noStroke();
+    fill(121, 191, 99);
+    stroke(100)
+    strokeWeight(1);
     rect(this.x,this.y,20,20);
-    if (acell === )
+    if (this.acell === 1){
+      noStroke()
+      fill(121, 191, 99);
+      rect(this.x, this.y-5, 20,5);
+      triangle(this.x+20,this.y-5,this.x+20,this.y,this.x+25, this.y-5);
+      triangle(this.x,this.y-5,this.x,this.y,this.x-5, this.y-5);
+    }
+    else if (this.acell === 2){
+      fill(250,200,120);
+      rect(this.x, this.y-5, 20,5);
+      triangle(this.x+20,this.y-5,this.x+20,this.y,this.x+25, this.y-5);
+      triangle(this.x,this.y-5,this.x,this.y,this.x-5, this.y-5);
+    }
+    if (this.bcell === 1){
+      fill(121, 191, 99);
+      rect(this.x+20, this.y, 5,20);
+      triangle(this.x+20,this.y,this.x+25,this.y-5,this.x+25, this.y);
+      triangle(this.x+20,this.y+20,this.x+25,this.y+20,this.x+25, this.y+25);
+    }
+    else if (this.bcell === 2){
+      fill(250,200,120);
+      rect(this.x+20, this.y, 5,20);
+      triangle(this.x+20,this.y,this.x+25,this.y-5,this.x+25, this.y);
+      triangle(this.x+20,this.y+20,this.x+25,this.y+20,this.x+25, this.y+25);
 
+    }
+    if (this.ccell === 1){
+      fill(121, 191, 99);
+      rect(this.x, this.y+20, 20,5);
+      triangle(this.x+20,this.y+20,this.x+20,this.y+25,this.x+25, this.y+25);
+      triangle(this.x,this.y+20,this.x,this.y+25,this.x-5, this.y+25);
+    }
+    else if (this.ccell === 2){
+      fill(250,200,120);
+      rect(this.x, this.y+20, 20,5);
+      triangle(this.x+20,this.y+20,this.x+20,this.y+25,this.x+25, this.y+25);
+      triangle(this.x,this.y+20,this.x,this.y+25,this.x-5, this.y+25);
+    }
+    if (this.dcell === 1){
+      fill(121, 191, 99);
+      rect(this.x-5, this.y, 5,20);
+      triangle(this.x,this.y,this.x-5,this.y-5,this.x-5, this.y);
+      triangle(this.x,this.y+20,this.x-5,this.y+20,this.x-5, this.y+25);
+    }
+    else if (this.dcell === 2){
+      fill(250,200,120);
+      rect(this.x-5, this.y, 5,20);
+      triangle(this.x,this.y,this.x-5,this.y-5,this.x-5, this.y);
+      triangle(this.x,this.y+20,this.x-5,this.y+20,this.x-5, this.y+25);
+    }
   }
 // Cell numbers mean ... 1 = basic/no change ... 2 = basic ocean/beach ... 3 = basic mountain/blend in
   
--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
+let grid = [];
+
+
 function setup() {
   createCanvas(1200, 1200);
   background(150,250,150);
+  for (cell in grid){}
 }
 
 function draw() {
