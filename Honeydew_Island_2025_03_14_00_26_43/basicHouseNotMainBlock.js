29b9b3f252476b0b053e1b810a93a6846c26689f
hector-bonnaud <hector.bonnaud28@trinityschoolnyc.org>
2025-04-08 21:44:14 -0400

    Add files via upload

---
+++ Honeydew_Island_2025_03_14_00_26_43/basicHouseNotMainBlock.js
@@ -0,0 +1,9 @@
+class basicHouseNotMainBlock {
+    constructor(positionx, positiony) {
+        this.x = positionx;
+        this.y = positiony;
+        this.type = 3;
+        this.hover = 0;
+        this.image = house;
+    }
+}
---
+++ Honeydew_Island_2025_03_14_00_26_43/basicWheat.js
@@ -0,0 +1,7 @@
+class basicWheat {
+    constructor(x, y) {
+        this.crop = "wheat";
+        this.cost = random(6, 12);
+        this.sellValue = random(8, 15);
+    }
+}
