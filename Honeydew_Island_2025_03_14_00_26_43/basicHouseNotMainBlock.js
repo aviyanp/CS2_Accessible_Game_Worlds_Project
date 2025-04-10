0f03231ec1b702029ba6070bd744cc083148907e
hector-bonnaud <hector.bonnaud28@trinityschoolnyc.org>
2025-04-10 13:29:16 -0400

    Delete Honeydew_Island_2025_03_14_00_26_43 directory

--- Honeydew_Island_2025_03_14_00_26_43/basicHouseNotMainBlock.js
+++
@@ -1,9 +0,0 @@
-class basicHouseNotMainBlock {
-    constructor(positionx, positiony) {
-        this.x = positionx;
-        this.y = positiony;
-        this.type = 3;
-        this.hover = 0;
-        this.image = house;
-    }
-}
--- Honeydew_Island_2025_03_14_00_26_43/basicWheat.js
+++
@@ -1,7 +0,0 @@
-class basicWheat {
-    constructor(x, y) {
-        this.crop = "wheat";
-        this.cost = random(6, 12);
-        this.sellValue = random(8, 15);
-    }
-}
